2171fc84e26be1e063c6312262c0226a244d6933
Shreesh-Gawande <shreeshgawande10@gmail.com>
2024-05-22 15:57:57 +0530

    Created hashed password

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express=require("express");
 const path=require("path");
+const userRoute=require("./routes/user");
 
 const app= express();
 const PORT=8000;
 
 app.set("view engine","ejs");
 app.set("views", path.resolve("./views"));
+app.use("/user",userRoute)
 
 app.get("/",(req,res)=>{
     return res.render("home");
--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const {Schema,model}= require("mongoose");
+const {createHmac,randomBytes}=require("crypto");        //createHmac helps us to hach the password
 
 const userSchema=new Schema({
 
@@ -29,6 +30,22 @@ const userSchema=new Schema({
         default:"User",
     }
 },{timestamps:true})
+
+//Process to hash a password
+userSchema.pre("save",function (next){           //This function runs before we have tosave any user
+    const user=this;
+
+    if(!user.isModified("password"))return;
+
+    const salt=randomBytes(16).toString();
+    const hashedPassword=createHmac("sha256",salt)
+    .update(user.password).digest("hex");
+
+    this.salt=salt;
+    this.passwor=hashedPassword;      //we update the password with the hashed password
+
+    next();
+})
 
 const User= model("user",userSchema);
 
