05971466c02f79ad262b96af3cd03d002f8af139
Shreesh-Gawande <shreeshgawande10@gmail.com>
2024-05-27 20:03:22 +0530

    Project completed

--- routes/blog.js
+++ routes/blog.js
@@ -3,6 +3,7 @@ const multer=require("multer");
 const path=require("path");
 
 const { Blog } = require('../models/blog');
+const { Comment } = require("../models/comment");
 
 const router= Router();
 const storage = multer.diskStorage({
@@ -32,11 +33,23 @@ router.post("/", upload.single("coverImage"),async (req,res)=>{
 return res.redirect(`/blog/${blog._id}`)
 })
 
+
+router.post("/comment/:blogId",async(req,res)=>{
+await Comment.create({
+content:req.body.content,
+blogId:req.params.blogId,
+createdBy:req.user._id,
+})
+return res.redirect(`/blog/${req.params.blogId}`);
+})
+
 router.get("/:id",async(req,res)=>{
+  const comments=await Comment.find({blogId:req.params.id}).populate("createdBy");
     const blog= await Blog.findById(req.params.id).populate("createdBy");
     return res.render("blog",{
         user:req.user,
         blog,
+        comments,
     })
 })
 
